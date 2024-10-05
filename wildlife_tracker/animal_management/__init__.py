99cf2b11a20e80eae2561173759c16f727c46264
zianxiang <157335028+zianxiang@users.noreply.github.com>
2024-10-05 15:41:10 -0400

    Add files via upload

---
+++ wildlife_tracker/animal_management/__init__.py
@@ -0,0 +1,4 @@
+from .animal_manager import AnimalManager
+from .animal import Animal
+
+__all__ = ['AnimalManager', 'Animal']
---
+++ wildlife_tracker/animal_management/animal_manager.py
@@ -0,0 +1,24 @@
+from typing import Any, Optional
+
+from wildlife_tracker.animal_management.animal import Animal
+
+class AnimalManager:
+
+    def __init__(self) -> None:
+        animals: dict[int, Animal] = {}
+
+    def get_animal_by_id(self, animal_id: int) -> Optional[Animal]:
+        pass
+
+    def register_animal(Animal) -> None:
+        pass
+
+    def remove_animal(animal_id: int) -> None:
+        pass
+
+    def get_animal_details(animal_id: int) -> dict[str, Any]:
+        pass
+
+    def update_animal_details(animal_id: int, **kwargs: Any) -> None:
+        pass
+
